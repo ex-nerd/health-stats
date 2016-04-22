9af60b29b737360ce2af19f35199b6774081f424
Chris Petersen <chris.c.petersen@nordstrom.com>
2016-04-22 00:20:41 -0700

    Add DailyCarbs graph

--- health_stats/reports/__init__.py
+++ health_stats/reports/__init__.py
@@ -1,11 +1,13 @@
 
 import graph.mean_glucose
+import graph.daily_carbs
 import table.full_summary
 import text.daily_summary
 
 Reports = {
     # Graphs
     'graph:MeanGlucose': graph.mean_glucose.MeanGlucose,
+    'graph:DailyCarbs': graph.daily_carbs.DailyCarbs,
     # Tables
     'table:FullSummary': table.full_summary.FullSummary,
     # Text logs
