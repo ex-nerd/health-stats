74d80c42c35ddf007bddfa80a65031f41c00d34a
Chris Petersen <chris.c.petersen@nordstrom.com>
2016-06-09 02:13:22 -0700

    Add a graph of daily insulin

--- health_stats/reports/__init__.py
+++ health_stats/reports/__init__.py
@@ -1,6 +1,7 @@
 
 import graph.mean_glucose
 import graph.daily_carbs
+import graph.daily_insulin
 import table.full_summary
 import text.daily_summary
 
@@ -8,6 +9,7 @@ Reports = {
     # Graphs
     'graph:MeanGlucose': graph.mean_glucose.MeanGlucose,
     'graph:DailyCarbs': graph.daily_carbs.DailyCarbs,
+    'graph:DailyInsulin': graph.daily_insulin.DailyInsulin,
     # Tables
     'table:FullSummary': table.full_summary.FullSummary,
     # Text logs
---
+++ health_stats/reports/graph/daily_insulin.py
@@ -0,0 +1,105 @@
+
+from datetime import datetime, time
+
+from plotly.offline import plot
+from plotly.graph_objs import Bar, Scatter
+
+from ..report import Report
+
+from health_stats.models.events import *
+from health_stats.database import DBSession
+from health_stats.dataset import DataSet
+
+
+class DailyInsulin(Report):
+
+    def render(self, output):
+        # Plot insulin values
+        long_x = []
+        long_y = []
+        rapid_x = []
+        rapid_y = []
+        day_x = []
+        day_y = []
+
+        started = False
+
+        session = DBSession()
+        events = session.query(InsulinEvent)
+        if self.start:
+            events = events.filter(Event.time >= "{}".format(self.start))
+        if self.end:
+            events = events.filter(Event.time <= "{}".format(self.end))
+        events = events.order_by(Event.time)
+        data = DataSet(
+            events,
+            lambda e: e.time.date()
+        )
+
+        for (day, daily_log) in data.group.items():
+
+            rapid_insulin = []
+            long_insulin = []
+
+            for event in daily_log:
+                started = True
+                if event.subtype == InsulinEvent.TYPE_LONG:
+                    long_insulin.append(int(event.value))
+                elif event.subtype == InsulinEvent.TYPE_RAPID:
+                    rapid_insulin.append(int(event.value))
+                else:
+                    raise Error("Undefined InsulinEvent subtype: {}".format(event.subtype))
+
+            # don't start counting days until we get at least one data point
+            if not started:
+                continue
+
+            long_x.append(datetime.combine(day, time(12, 0, 0, 0)))
+            rapid_x.append(datetime.combine(day, time(12, 0, 0, 0)))
+            day_x.append(datetime.combine(day, time(12, 0, 0, 0)))
+            long_y.append(sum(long_insulin) if long_insulin else None)
+            rapid_y.append(sum(rapid_insulin) if rapid_insulin else None)
+
+            day_total = 0
+            if long_y[-1]:
+                day_total += long_y[-1]
+            if rapid_y[-1]:
+                day_total += rapid_y[-1]
+
+            day_y.append(day_total if day_total else None)
+
+        plot(
+            {
+                'data': [
+                    Bar(
+                        name="Long",
+                        x=long_x, y=long_y,
+                    ),
+                    Bar(
+                        name="Rapid",
+                        x=rapid_x, y=rapid_y,
+                    ),
+                    Scatter(
+                        name="Total",
+                        x=day_x, y=day_y,
+                        text=day_y,
+                        mode='markers+text',
+                        marker={
+                            'size': 30,
+                        },
+                        textfont={
+                            'color': 'white',
+                        },
+                        hoverinfo='none',
+                        showlegend=False,
+                    ),
+                ],
+                'layout': {
+                    'xaxis': {'title': 'Date'},
+                    'yaxis': {'title': 'Insulin (u)'},
+                    'barmode': 'stack',
+                },
+            },
+            auto_open=False,
+            filename=output
+        )
